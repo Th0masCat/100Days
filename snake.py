3ddd279c8a4d2e7013dc44507dbcc3d82ddbe745
Th0masCat <catsaredogstoo@gmail.com>
2022-06-22 22:13:30 +0530

    finished snake

--- snake.py
+++ snake.py
@@ -10,11 +10,14 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            turtle_object = Turtle(shape="square")
-            turtle_object.color("white")
-            turtle_object.penup()
-            turtle_object.goto(position)
-            self.segments.append(turtle_object)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -24,6 +27,9 @@ class Snake:
             self.segments[seg_num].goto(x_corr, y_corr)
 
         self.segments[0].forward(MOVE_SPEED)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def up(self):
         if self.segments[0].heading != 270:
--- snake_game.py
+++ snake_game.py
@@ -1,6 +1,8 @@
 from turtle import Screen, Turtle
 import time
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 
@@ -10,6 +12,8 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -24,5 +28,21 @@ while not gameOver:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.segments[0].distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    if snake.segments[0].xcor() > 280 or snake.segments[0].xcor() < -280 or snake.segments[0].ycor() > 280 or snake.segments[0].ycor() < -280:
+        gameOver = True
+        scoreboard.game_over()
+
+    for segment in snake.segments:
+        if segment == snake.segments[0]:
+            pass
+        elif snake.segments[0].distance(segment) < 10:
+            gameOver = True
+            scoreboard.game_over()
 
 screen.exitonclick()
